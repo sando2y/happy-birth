358410aaba5f034defac44648d3174438e1b72e7
sando2y <sandyyousef188@gmail.com>
2024-06-27 22:21:58 +0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,18 +1,16 @@
-
-
-document.getElementById("subBtn").addEventListener('click',()=>{
-    if (soulValidation()) {
-        location.pathname = '/home.html'
-    }
-})
-function soulValidation(){
-    const regex = /^111$/
-    const inputSoul = document.querySelector('input')
-    if(regex.test(inputSoul.value)) {
-        return true
-    }else {
-        inputSoul.classList.add('is-invalid')
-        return false
-    }
+document.getElementById("subBtn").addEventListener("click", (e) => {
+  if (soulValidation()) {
+    console.log(e.target.setAttribute("href", "home.html"));
+  }
+});
+function soulValidation() {
+  const regex = /^111$/;
+  const inputSoul = document.querySelector("input");
+  if (regex.test(inputSoul.value)) {
+    return true;
+  } else {
+    inputSoul.classList.add("is-invalid");
+    return false;
+  }
 }
 console.log(location.pathname);
